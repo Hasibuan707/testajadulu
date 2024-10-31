7d47b4a06ae4b95863a09a3259eef66e607f1d7b
email <idrushasibuan20@gmail.com>
2024-10-31 11:12:01 +0700

    looping bersarang

---
+++ test.js
@@ -0,0 +1,81 @@
+// // // let nama: string = "ahmad";
+// // // let age: number = 28;
+// // // let hobies: [string, string, string];
+// // // hobies = ["bola", "basket", "catur"];
+// // // console.log(nama);
+// // // console.log(age);
+// // // console.log(hobies);
+// // // TIPE DATA CUSTOM
+// // type TemanType = {
+// //     nama: string;
+// //     gender: string;
+// //     goodLooking: boolean;
+// // }
+// // let temanKita: TemanType;
+// // temanKita = {
+// //     nama: "Ahmad,"
+// //     gender: "Laki-laki"
+// //     goodLooking: true,
+// // };
+// // console.log(temanKita);
+// // Function
+// function create() {
+//   return;
+// }
+// console.log(create());
+// no 1 exercise
+// let n = 9;
+// for (let i = 1; i <= 10; ++i) console.log(n + " * " + i + " = " + n * i);
+// console.log(n);
+// //no 2 exercise
+// function isPalindrome(s: string): boolean {}
+// // no 3 exercise
+// let res = (100000 * 0.001) / 100;
+// console.log(res + "km");
+// no 4 exercise
+// const price = 1000;
+// let IDR = new Intl.NumberFormat("IDR", {
+//   style: "currency",
+//   currency: "Rp",
+// });
+// console.log(`The formated version of ${price} is ${IDR.format(price)}`);
+// const formatPrice = (amount: number): string => {
+//   return Intl.NumberFormat("ne-NP", {
+//     style: "currency",
+//     currency: "NPR",
+//   }).format(amount);
+// };
+// export default formatPrice;
+var a = "a";
+a = a + "b";
+console.log(a);
+a += "c";
+a += "d";
+a += "ef";
+a += "gh";
+console.log(a);
+console.log(a(3));
+// let input = 5;
+// result = "";
+// for (let i = 1; i <= input; i++) {
+//   result += "*";
+// }
+// for (let j = 1; j <= input; j++) {
+//   console.log(result);
+// }
+// let result2 = "";
+// for (let k = 1; k < input; k++) {
+//   result2 += "*";
+//   console.log(result2);
+// }
+// let result2 = "";
+// for (let k; k <= input; k++) {
+//   result2 = "*";
+//   console.log(result2);
+// }
+// for (let i = 0; i <= input; i++) {
+//   let result = ""; // deklarasi dan asssign ""
+//   result += "*"; // setelah itu ditambah / di add  "*"
+//   console.log(result); // cetak atau tampilkan
+// }
+console.log(a);
